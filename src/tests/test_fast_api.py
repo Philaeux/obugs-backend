47828a7c3166457d09d6eef03d5058d555b0e54c
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2024-05-24 00:16:08 +0200

    Migration of graphql tests to generic fastapi tests

--- src/tests/test_fast_api.py
+++ src/tests/test_fast_api.py
@@ -3,6 +3,7 @@ import pytest
 from fastapi.testclient import TestClient
 
 from obugs.backend import app
+from obugs.database.database import Database
 from obugs.settings import Settings
 
 
@@ -10,7 +11,8 @@ from obugs.settings import Settings
 def mock_app():
     """Prepare a mock application to run REST tests"""
     settings = Settings()
-    settings.database_uri = "sqlite:///:memory:"
+    settings.database_uri = "sqlite+pysqlite:///sqlite.db"
+    Database(uri=settings.database_uri, check_migrations=True)
     yield TestClient(app)
 
 
@@ -19,3 +21,22 @@ def test_rest_hello_world(mock_app):
     response = mock_app.get('/hello')
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_query_softwares(mock_app):
+    """Example of a graphql query softwares"""
+    query = """
+    {
+        softwares(search: null){
+            id
+        }
+    }
+    """
+    response = mock_app.post("/graphql", json={"query": query})
+    print(response)
+    assert response.status_code == 200
+    json = response.json()
+    print(json)
+    assert "data" in json
+    assert "softwares" in json["data"]
+    assert len(json["data"]["softwares"]) >= 0
